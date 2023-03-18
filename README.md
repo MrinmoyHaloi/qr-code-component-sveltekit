ab44b1ff3c2740b47bc1ed3536d61913998b5bcb
Mrinmoy Haloi <69071143+MrinmoyHaloi@users.noreply.github.com>
2023-03-18 21:58:16 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,57 @@
+# Frontend Mentor - QR code component solution
+
+This is a solution to the [QR code component challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/qr-code-component-iux_sIO_H). Frontend Mentor challenges help you improve your coding skills by building realistic projects. 
+
+## Table of contents
+
+- [Overview](#overview)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+- [My process](#my-process)
+  - [Built with](#built-with)
+  - [What I learned](#what-i-learned)
+- [Author](#author)
+
+## Overview
+
+### Screenshot
+
+![](./screenshot.png)
+
+### Links
+
+- Solution URL: [Add solution URL here](https://your-solution-url.com)
+- Live Site URL: [Add live site URL here](https://your-live-site-url.com)
+
+## My process
+
+### Built with
+
+- Sveltekit
+- SCSS
+- Flexbox
+- CSS Grid
+
+### What I learned
+
+This project was relatively easy to complete. I didn't learnt much but I learnt about frontend mentor and how to use it. Looking forward to complete more challenges.
+
+## Author
+
+- Website - [mrinmoy.is-a.dev](https://mrinmoy.is-a.dev)
+- Frontend Mentor - [@MrinmoyHaloi](https://www.frontendmentor.io/profile/MrinmoyHaloi)
+- Twitter - [@_mrinmoy_haloi_](https://www.twitter.com/_mrinmoy_haloi_)
+
+
+## Developing
+
+### Install dependencies
+```bash
+pnpm install
+```
+
+### Start the dev server
+
+```bash
+pnpm dev
+```
---
+++ src/routes/+page.svelte
@@ -0,0 +1,69 @@
+<div class="container">
+	<div class="qr-component">
+		<img src="image-qr-code.png" alt="qr code" />
+		<div class="text">
+			<h1>Improve your front-end skills by building projects</h1>
+			<p>Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</p>
+		</div>
+	</div>
+	<div class="attribution">
+		Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank"
+			>Frontend Mentor</a
+		>. Programmed by <a href="https://mrinmoy.is-a.dev">Mrinmoy Haloi</a>.
+	</div>
+</div>
+
+<style lang="scss">
+	.container {
+		background-color: hsl(212, 45%, 89%);
+		height: 100vh;
+		width: 100vw;
+		padding: 1rem;
+		display: grid;
+		place-items: center;
+
+		.qr-component {
+			background-color: hsl(0, 0%, 100%);
+			max-width: 16rem;
+			padding: 0.8rem;
+			border-radius: 1rem;
+			text-align: center;
+			box-shadow: 0px 10px 20px 0px rgba(0, 0, 0, 0.1);
+
+			img {
+				width: 100%;
+				object-fit: cover;
+				border-radius: 0.5rem;
+			}
+
+			.text {
+				padding: 0.5rem;
+				h1 {
+					font-size: 1.1rem;
+					font-weight: 700;
+					margin-block: 0.8rem;
+					color: hsl(218, 44%, 22%);
+				}
+
+				p {
+					font-size: 12px;
+					margin-bottom: 0.7rem;
+					padding-inline: 0.1rem;
+					color: hsl(220, 15%, 55%);
+				}
+			}
+		}
+	}
+
+	.attribution {
+		font-size: 12px;
+		position: absolute;
+		bottom: 2rem;
+		right: 2rem;
+        padding: 1rem;
+	}
+	.attribution a {
+		color: hsl(214, 68%, 45%);
+		text-decoration: none;
+	}
+</style>
